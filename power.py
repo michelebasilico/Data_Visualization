85e93ead262e55bf2e55dd4ed23adb0d1ad8c654
michelebasilico <39620103+michelebasilico@users.noreply.github.com>
2020-05-11 15:43:09 +0200

    Add files via upload

--- power.py
+++ power.py
@@ -1,16 +1,6 @@
 import pandas
 import datetime as dt
-
-#Definiamo una funzione in grado di convertire il tempo Unix in un formato Data e ritorna le ore di tale formato.In ingresso vuole un dataframe(Unix,Energia)
-def Unix_to_hours(dataframe):
-    #Convertiamo epochUnix nel formato data
-    dataframe['date'] = pandas.to_datetime(dataframe['date'], origin='unix',unit = 's')
-
-    #Estraggo le ore
-    ore = dataframe['date'].dt.time
-
-    return ore
-
+from bokeh.models import DatetimeTickFormatter
 
 #Definiamo una funzione per il calcolo della potenza
 def calc_Pot(tempo,energia):
@@ -38,7 +28,7 @@ def calc_Pot(tempo,energia):
     return potenza
 
 def data_cleaning(df,date,start = '',end = '',freq = '5T'):
-    
+
     #Per BackGroundLoad che comincia da 0 -> Aggiungo la data del 12 dicembre 2015 00:00:00
     if(df['date'][0] == 0):
         df['date'] = df['date'] + 1449882000
@@ -61,7 +51,7 @@ def data_cleaning(df,date,start = '',end = '',freq = '5T'):
     #Genero la lista dell'energia(i dati sono puliti, per ogni minuto) e delle ore
     energy = new_df['energy'].tolist()
     
-    data = {'date': new_df['date'], 'energy': energy}
+    data = {'date': new_df['date'], 'energy': energy }
     dataframe = pandas.DataFrame(data)
     return dataframe
 
@@ -76,34 +66,57 @@ def dfEnergy_to_dfPower(df):
     ultimo_elemento = len(new_df) - 1
     new_df = new_df.drop([ultimo_elemento])
     #Creo il dataframe del singolo file e lo aggiungo alla lista a secondo della tipologia
-    data = {'date': new_df['date'], 'power': power}
+    data = {'home': new_df['home'],'date': new_df['date'], 'power': power }
     dataframe = pandas.DataFrame(data)
     return dataframe
 
-def sum_power(list_df):
+def sum_pow_en(list_df,powORen,home = False):
     #Somma di una lista di dataframe(sull'intera giornata)
     #Concateno i dataframe
     df_new = pandas.concat(list_df)
-
-    #Li raggruppo in base alla data e sommo le potenze(con data uguale)
-    df_new = df_new.groupby(df_new['date'],as_index=False,sort=True)[['power']].sum()
+    if(home):
+        df_new = df_new.groupby([df_new['home'],df_new['date']],as_index=False,sort=True)[[powORen]].sum()
+    else:
+        #Resolution Bug -> Potrebbe avere effetti collaterali 
+        df_new = df_new.fillna(0)
+        #Attenzione elimina la colonna home dal dataframe. Si cade in questo caso solo quando si effettuano calcoli sul vicinato. In
+        #tal senso la colonna home non ha più nessun valore. Attenziona all'utilizzo del dataframe che ritorna
+        df_new = df_new.groupby(df_new['date'],as_index=False,sort=True)[[powORen]].sum()
 
     return df_new
 
-def diff_power(df1,df2):
+def diff_pow_en(df1,df2,powORen):
     #Differenza tra potenze di due dataframe(sull'intera giornata)
-    df2['power'] *= -1
-    df_new = [df1,df2]
-    df_final = sum_power(df_new)
-    df2['power'] *= -1
+    df2_new = df2.copy()
+    df2_new[powORen] *= -1
+    df_new = [df1,df2_new]
+    df_final = sum_pow_en(df_new,powORen)
     return df_final
 
-def self_consumption(df_ProdTot,df_ConsTot):
-    #Calcolo autoconsumo (PotenzaProdotta-PotenzaImmessaNellaRete)
+def stored_pow_en(df_ProdTot,df_ConsTot,powORen):
+   #Definisco la PotenzaImmessaNellaRete(PotenzaProdotta-PotenzaConsumata senza valori negativi)
+   #Diff chiama anche la somma. Questa somma, chiamata senza home, fa perdere la colonna al dataframe
+    df_Stored = diff_pow_en(df_ProdTot,df_ConsTot,powORen)
+    df_Stored[powORen] = df_Stored[powORen].mask(df_Stored[powORen] < 0, 0)
+    return df_Stored
 
-    #Definisco la PotenzaImmessaNellaRete(PotenzaProdotto-PotenzaConsumata senza valori negativi)
-    df_PotImm = diff_power(df_ProdTot,df_ConsTot)
-    df_PotImm['power'] = df_PotImm['power'].mask(df_PotImm['power'] < 0, 0)
+def self_consumption(df_ProdTot,df_ConsTot,powORen):
+    #Attenzione!! Il calcolo dell'autoncosumo comporta la perdita della colonna home
+    #Calcolo autoconsumo (PotenzaProdotta-PotenzaImmessaNellaRete)
+    #Potenza Immessa nella rete
+    df_Stored = stored_pow_en(df_ProdTot,df_ConsTot,powORen)
     #Calcolo Autoconsumo
-    df_selfConsumption = diff_power(df_ProdTot,df_PotImm)
+    df_selfConsumption = diff_pow_en(df_ProdTot,df_Stored,powORen)
     return df_selfConsumption
+
+def plot_axis_dateFormatter(plot_Figure):
+    #Imposta il formato data per l'ascisse del grafico passato come argomento
+    plot_Figure.xaxis.formatter = DatetimeTickFormatter(
+        
+            years="20%y/%m/%d %H:%M",
+            days="20%y/%m/%d %H:%M",
+            months="20%y/%m/%d %H:%M",
+            hours="20%y/%m/%d %H:%M",
+            minutes="20%y/%m/%d %H:%M"
+                
+        )
